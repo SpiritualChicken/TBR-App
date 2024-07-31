cabd623432da098d557b1c8462d9e44183f5c0d9
Charlie Newlinds <cnewlinds@gmail.com>
2024-07-31 17:26:32 +1000

    Initial entity models

--- server/models.py
+++ server/models.py
@@ -3,4 +3,45 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    tbr_books = db.relationship('TBR', back_populates='user', cascade='all, delete-orphan')
+    reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    
+    tbr_users = db.relationship('TBR', back_populates='book', cascade='all, delete-orphan')
+    reviews = db.relationship('Review', back_populates='book', cascade='all, delete-orphan')
+
+class TBR(db.Model, SerializerMixin):
+    __tablename__ = 'tbr'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    
+    user = db.relationship('User', back_populates='tbr_books')
+    book = db.relationship('Book', back_populates='tbr_users')
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    review_text = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    
+    user = db.relationship('User', back_populates='reviews')
+    book = db.relationship('Book', back_populates='reviews')
