4fed9b611e1c5420e214d08bccb4c72f49b0783d
Charlie Newlinds <cnewlinds@gmail.com>
2024-08-02 16:29:45 +1000

    add server routes

--- server/app.py
+++ server/app.py
@@ -3,12 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User, Book, TBR, Review
 
 
 # Views go here!
@@ -16,6 +17,43 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+class UsersResource(Resource):
+    def get(self):
+        users = User.query.all()
+        return jsonify([user.serialize() for user in users])
+
+class BooksResource(Resource):
+    def get(self):
+        books = Book.query.all()
+        return jsonify([book.serialize() for book in books])
+
+class TBRResource(Resource):
+    def get(self):
+        tbr_entries = TBR.query.all()
+        return jsonify([{
+            'user_id': tbr.user_id,
+            'book_id': tbr.book_id,
+            'username': tbr.user.username,
+            'book_title': tbr.book.title
+        } for tbr in tbr_entries])
+
+class ReviewsResource(Resource):
+    def get(self):
+        reviews = Review.query.all()
+        return jsonify([{
+            'user_id': review.user_id,
+            'book_id': review.book_id,
+            'rating': review.rating,
+            'review_text': review.review_text,
+            'username': review.user.username,
+            'book_title': review.book.title
+        } for review in reviews])
+
+api.add_resource(UsersResource, '/users')
+api.add_resource(BooksResource, '/books')
+api.add_resource(TBRResource, '/tbr')
+api.add_resource(ReviewsResource, '/reviews')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -14,12 +14,12 @@ class User(db.Model, SerializerMixin):
     tbr_books = db.relationship('TBR', back_populates='user', cascade='all, delete-orphan')
     reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
 
-    def __repr__(self): 
-        return f"""
-            username: {self.username}
-            email: {self.email}
-            password: {self.password}
-        """
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
 
 class Book(db.Model, SerializerMixin):
     __tablename__ = 'books'
@@ -27,6 +27,13 @@ class Book(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+        }
     
     tbr_users = db.relationship('TBR', back_populates='book', cascade='all, delete-orphan')
     reviews = db.relationship('Review', back_populates='book', cascade='all, delete-orphan')
@@ -41,14 +48,34 @@ class TBR(db.Model, SerializerMixin):
     user = db.relationship('User', back_populates='tbr_books')
     book = db.relationship('Book', back_populates='tbr_users')
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'book_id': self.book_id,
+            'username': self.user.username,
+            'book_title': self.book.title
+        }
+
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
     
     id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
     review_text = db.Column(db.Text, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'rating': self.rating,
+            'review_text': self.review_text,
+            'user_id': self.user_id,
+            'book_id': self.book_id,
+            'username': self.user.username,
+            'book_title': self.book.title
+        }
     
     user = db.relationship('User', back_populates='reviews')
     book = db.relationship('Book', back_populates='reviews')
